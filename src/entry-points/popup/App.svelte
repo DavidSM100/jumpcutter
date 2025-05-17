d82613098642a992eaf9ddc70d3ba29c2300a6fe
DavidSM100 <davidsm@systemli.org>
2025-05-17 19:40:18 -0400

    style(popup): Remove emoji in local file player link for a more mature appearance

--- src/entry-points/popup/App.svelte
+++ src/entry-points/popup/App.svelte
@@ -1004,7 +1004,7 @@ along with Jump Cutter Browser Extension.  If not, see <https://www.gnu.org/lice
         {...openLocalFileLinkProps}
         on:click={onClickOpenLocalFileLink}
         style="margin-top: 1rem;"
-      >📂 {getMessage('openLocalFile')}</a>
+      >{getMessage('openLocalFile')}</a>
     {/if}
     <label
       style="
